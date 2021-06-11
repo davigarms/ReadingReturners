1dd868c93a7702fd4e919180e960c7afa320cee2
Davi Garms <davigarms@gmail.com>
2021-06-11 17:01:41 +0100

    Fix key warning in the books list and remove PropTypes shape on the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,10 @@ export default function Index({ books }) {
       {books.length > 0 ? (
         books.map((book) => {
           const content = (
-            <div>
-              <li key={book._id}>
-                <h3>{book.title}</h3>
-                <h4>{book.author}</h4>
-              </li>
-            </div>
+            <li key={book._id}>
+              <h3>{book.title}</h3>
+              <h4>{book.author}</h4>
+            </li>
           )
           return content
         })
@@ -33,11 +31,5 @@ export async function getStaticProps() {
 }
 
 Index.propTypes = {
-  books: PropTypes.arrayOf([
-    PropTypes.shape({
-      _id: PropTypes.string,
-      title: PropTypes.string,
-      author: PropTypes.string,
-    }),
-  ]).isRequired,
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
