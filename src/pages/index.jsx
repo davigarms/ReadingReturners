691a43bba8b990aa55c0dbb01e2370a3b80c4803
Davi Garms <davigarms@gmail.com>
2021-06-16 13:41:58 +0100

    Add and config Styled Components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,24 @@
 import { fetchAPI } from 'lib/api'
 import { PropTypes } from 'prop-types'
+import styled from 'styled-components'
 import { spacing, colors, fontWeights } from 'styles/dictionary'
+
+const List = styled.ul`
+  padding: ${spacing.small};
+  background-color: ${colors.blue1};
+  color: ${colors.white};
+  li {
+    margin: ${spacing.small};
+  }
+
+  h3 {
+    font-weight: ${fontWeights.bold};
+  }
+`
 
 export default function Index({ books }) {
   return (
-    <ul>
+    <List>
       {books.length > 0 ? (
         books.map((book) => {
           const content = (
@@ -20,22 +34,7 @@ export default function Index({ books }) {
           <h3>No books found</h3>
         </li>
       )}
-      <style jsx>
-        {`
-          ul {
-            padding: ${spacing.small};
-            background-color: ${colors.yellow};
-            li {
-              margin: ${spacing.small};
-            }
-          }
-
-          h3 {
-            font-weight: ${fontWeights.bold};
-          }
-        `}
-      </style>
-    </ul>
+    </List>
   )
 }
 
