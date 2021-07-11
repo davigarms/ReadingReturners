be3444a45777b15e32fdd3c40827dbab5258db8f
Davi Garms <davigarms@gmail.com>
2021-07-11 19:12:59 +0100

    Refactor getData API

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,31 +1,22 @@
 import { useCallback, useState } from 'react'
 
 export function getURL(path = '') {
-  return `${
-    process.env.APP_URL || 'http://localhost:3000'
-  }/api${path}`
+  return `${process.env.APP_URL || 'http://localhost:3000'}/api${path}`
 }
 
-export function useGetData(path, id) {
+export function useGetData(path) {
   const [data, setData] = useState()
 
   const getData = useCallback(async () => {
-    const loadedData = (await fetchData(`${path}/${id}`)) || {}
-    loadedData && loadedData.length === 0
-      ? setData(loadedData)
-      : setData(loadedData[0])
-  }, [path, id])
+    const requestUrl = getURL(path)
+    try {
+      const response = await fetch(requestUrl)
+      const loadedData = await response.json()
+      loadedData && setData(loadedData)
+    } catch (error) {
+      //error treatment
+    }
+  }, [path])
 
   return [data, getData]
-}
-
-export async function fetchData(path) {
-  const requestUrl = getURL(path)
-  try {
-    const response = await fetch(requestUrl)
-    const data = await response.json()
-    return data
-  } catch (error) {
-    return error
-  }
 }
